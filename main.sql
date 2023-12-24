fa23a2827d3128de748370da7fee3255948aa23d
phaneendra37 <kphaneendra337@gmail.com>
2023-12-24 16:32:09 +0530

    UPDATED

--- main.sql
+++ main.sql
@@ -1,5 +1,5 @@
 -- Will map the triggers here
 -- will map the tables after the triggers
-\ i './database/formers/profile.sql';
+'\i' './database/formers/profile.sql';
 
 -- will map the views here after rendering the tables
